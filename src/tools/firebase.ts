9f8812f0fbfcd010b5455eef019e0b691e96951e
ssinha98 <ssinha20@cmc.edu>
2025-02-18 11:22:16 +0530

    redoing auth

--- src/tools/firebase.ts
+++ src/tools/firebase.ts
@@ -5,18 +5,19 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA_y1WhNZ1lM3jmX8a7fs-hB3GlC9QPNcw",
-    authDomain: "notebookmvp.firebaseapp.com",
-    projectId: "notebookmvp",
-    storageBucket: "notebookmvp.firebasestorage.app",
-    messagingSenderId: "326873913382",
-    appId: "1:326873913382:web:59c21b4ea3651a428aaee7",
-    measurementId: "G-NZY8WE3848"
-  };
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
-  // Initialize Firebase Authentication and get a reference to the service
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export default app;
